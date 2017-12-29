752dc308c96d5d71d624ff419284856f653a22b1
kwarodom <kwarodom@vt.edu>
2017-12-29 18:43:07 +0700

    adding lifecycle methods and setting state

--- process/js/app.js
+++ process/js/app.js
@@ -8,8 +8,8 @@ var MainInterface = React.createClass({
         } //return
     }, //getInitialState
 
-    componentDidMount:function () {
-        this.serverRequest = $.get('./js/data.json', function (result) {
+    componentDidMount: function() {
+        this.serverRequest = $.get('./js/data.json', function(result) {
             var tempApts = result;
             this.setState({
                 myAppointments: tempApts
@@ -17,40 +17,32 @@ var MainInterface = React.createClass({
         }.bind(this));
     },
 
-    componentWillUnmount: function () {
+    componentWillUnmount: function() {
         this.serverRequest.abort();
     },
 
     render: function() {
         var filteredApts = this.state.myAppointments;
-        filteredApts = filteredApts.map(function(item, index){
-            console.log(this);
-            return (
+        filteredApts = filteredApts.map(function(item, index) {
+            return(
                 <li className="pet-item media" key={index}>
-                    <div className="pet-head">
-                        <span className="pet-name"> {this.state.myAppointments[index].petName} </span>
-                        <span className="apt-date pull-right"> {this.state.myAppointments[index].aptDate} </span>
-                    </div>
-                    <div className="owner-name">
-                        <span className="label-item">Owner: </span>
-                        { this.state.myAppointments[index].ownerName }
-                    </div>
-                    <div className="apt-notes">
-                        { this.state.myAppointments[index].aptNotes }
+                    <div className="pet-info media-body">
+                        <div className="pet-head">
+                            <span className="pet-name">{this.state.myAppointments[index].petName}</span>
+                            <span className="apt-date pull-right">{this.state.myAppointments[index].aptDate}</span>
+                        </div>
+                        <div className="owner-name"><span className="label-item">Owner:</span>
+                            {this.state.myAppointments[index].ownerName}</div>
+                        <div className="apt-notes">{this.state.myAppointments[index].aptNotes}</div>
                     </div>
                 </li>
             ) //return
-        }.bind(this)); //filteredApts
-
+        }.bind(this)); //filteredApts.map
         return (
             <div className="interface">
-                <div className="item-list media-list">
-                    <ul className="item-list media-list">
-                        { filteredApts }
-                    </ul>
-                </div>
+                <ul className="item-list media-list">{filteredApts}</ul>
             </div>
-        )
+        ) //return
     } //render
 }); //MainInterface
 
