f53b577e917a7836c373237852b894fc00974d62
kwarodom <kwarodom@vt.edu>
2017-12-29 18:20:02 +0700

    adding lifecycle methods

--- process/js/app.js
+++ process/js/app.js
@@ -2,30 +2,53 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var MainInterface = React.createClass({
-    getInitialState: function () {
+    getInitialState: function() {
         return {
-            title: 'Appointments',
-            show: true
-        } // return
+            myAppointments: []
+        } //return
     }, //getInitialState
-    render: function() {
-        var showTitle;
-        if (this.state.show) {
-            showTitle: 'New'
-        }
 
-        var displayList = {
-            display: this.state.show ? 'block':'none',
-            color:'red'
-        };
+    componentDidMount:function () {
+        this.serverRequest = $.get('./js/data.json', function (result) {
+            var tempApts = result;
+            this.setState({
+                myAppointments: tempApts
+            }); //setState
+        }.bind(this));
+    },
+
+    componentWillUnmount: function () {
+        this.serverRequest.abort();
+    },
+
+    render: function() {
+        var filteredApts = this.state.myAppointments;
+        filteredApts = filteredApts.map(function(item, index){
+            console.log(this);
+            return (
+                <li className="pet-item media" key={index}>
+                    <div className="pet-head">
+                        <span className="pet-name"> {this.state.myAppointments[index].petName} </span>
+                        <span className="apt-date pull-right"> {this.state.myAppointments[index].aptDate} </span>
+                    </div>
+                    <div className="owner-name">
+                        <span className="label-item">Owner: </span>
+                        { this.state.myAppointments[index].ownerName }
+                    </div>
+                    <div className="apt-notes">
+                        { this.state.myAppointments[index].aptNotes }
+                    </div>
+                </li>
+            ) //return
+        }.bind(this)); //filteredApts
+
         return (
             <div className="interface">
-                <h1> { showTitle } {this.state.title} </h1>
-                <ul style={displayList}>
-                    <li>Buffy 3:30 PM</li>
-                    <li>Spot 8:30 PM</li>
-                    <li>Goldie 3:50 PM</li>
-                </ul>
+                <div className="item-list media-list">
+                    <ul className="item-list media-list">
+                        { filteredApts }
+                    </ul>
+                </div>
             </div>
         )
     } //render
